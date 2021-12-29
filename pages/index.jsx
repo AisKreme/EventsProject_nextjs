7656722bc9bf1c26175567386e3d51b99f63891c
Maik Jüngling <juengling.maik@gmail.com>
2021-12-29 10:40:28 +0100

    add EventList to HomePage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
+
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>THe Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
