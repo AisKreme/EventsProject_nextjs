be0d10d9e39c472353b15a0e19dd56cece9b2242
Maik Jüngling <juengling.maik@gmail.com>
2021-12-29 13:44:40 +0100

    add event detail compoinents

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,8 +1,33 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventContent from "../../components/event-detail/EventContent";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventSummary from "../../components/event-detail/EventSummary";
+import { getEventById } from "../../dummy-data";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  console.log(eventId);
+  const event = getEventById(eventId);
+  console.log(event);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.imageAlt}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
